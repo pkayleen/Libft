664010abc37c8f1bc254308ee945d6b8a0241441
Profit Kayleen <pkayleen@at-n1.msk.21-school.ru>
2020-11-09 14:45:31 +0300

    Standard Library (Completed)

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: pkayleen <pkayleen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 18:22:08 by pkayleen          #+#    #+#             */
-/*   Updated: 2020/11/04 18:36:35 by pkayleen         ###   ########.fr       */
+/*   Updated: 2020/11/04 21:45:13 by pkayleen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,14 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t n)
 {
 	int	i;
 
+	if (src == NULL)
+		return (0);
+	if (n == 0 || dst == NULL)
+	{
+		return (ft_strlen(src));
+	}
 	i = 0;
-	while (n != 1 && src[i] != '\0')
+	while (n > 1 && src[i] != '\0')
 	{
 		n--;
 		dst[i] = src[i];
