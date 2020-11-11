9ed9c34f4d17ee69aa12997b9f102a4a2bc54be4
Profit Kayleen <pkayleen@at-n1.msk.21-school.ru>
2020-11-11 21:00:45 +0300

    Standard Library (Completed)

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: pkayleen <pkayleen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/30 17:04:50 by pkayleen          #+#    #+#             */
-/*   Updated: 2020/11/03 15:39:59 by pkayleen         ###   ########.fr       */
+/*   Updated: 2020/11/11 18:33:13 by pkayleen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,20 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int res;
-	int	i;
+	unsigned char	i;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
+	str1 = (unsigned char	*)s1;
+	str2 = (unsigned char	*)s2;
 	i = 0;
-	while (*s1 != '\0' && *s2 != '\0' && n > 0)
+	while ((*str1) && (*str2) && (i < n))
 	{
-		n--;
-		if (s1[i] == s2[i] && n != 0)
-		{
-			i++;
-		}
+		str1++;
+		str2++;
+		i++;
+		if (*str1 != *str2)
+			return (*str1 - *str2);
 	}
-	res = s1[i] - s2[i];
-	return (res);
+	return (*str1 - *str2);
 }
