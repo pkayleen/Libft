4d02d0ffc380c9399c04e435a271115961ec2fb3
Profit Kayleen <pkayleen@am-c3.msk.21-school.ru>
2020-11-15 20:51:17 +0300

    Standard Library (Completed)

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: pkayleen <pkayleen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 17:02:21 by pkayleen          #+#    #+#             */
-/*   Updated: 2020/11/11 16:51:27 by pkayleen         ###   ########.fr       */
+/*   Updated: 2020/11/15 15:18:27 by pkayleen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,27 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize_buf)
 {
 	unsigned int	i;
-	unsigned int	len1;
 	unsigned int	len2;
 
 	i = 0;
-	len1 = 0;
+	if (!dstsize_buf)
+		return (ft_strlen(src));
 	if (dstsize_buf == 0)
 		return (ft_strlen(dst));
 	while ((dst[i] != '\0') && (i < dstsize_buf))
 		i++;
-	len1 = i;
 	len2 = 0;
 	if (i >= dstsize_buf)
 	{
 		dst[ft_strlen(dst)] = '\0';
-		return (len1 + ft_strlen(src));
+		return (i + ft_strlen(src));
 	}
-	while ((src[len2] != '\0') && (len2 < dstsize_buf - len1 - 1))
+	while ((src[len2] != '\0') && (len2 < dstsize_buf - i - 1))
 	{
 		dst[i + len2] = src[len2];
 		len2++;
 	}
 	if (i < dstsize_buf)
 		dst[i + len2] = '\0';
-	return (len1 + ft_strlen(src));
+	return (i + ft_strlen(src));
 }
