aeb3e19da3f18ac4e49b9e0275753997c11e9a21
Profit Kayleen <pkayleen@at-d3.msk.21-school.ru>
2020-11-18 11:04:41 +0300

    Additional function (Updated)

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,50 +6,37 @@
 /*   By: pkayleen <pkayleen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/14 00:29:56 by pkayleen          #+#    #+#             */
-/*   Updated: 2020/11/18 00:32:01 by pkayleen         ###   ########.fr       */
+/*   Updated: 2020/11/18 09:18:47 by pkayleen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	check_endset(char *str, char *set)
+char					*ft_strtrim(char const *s1, char const *set)
 {
-	int	i;
-
-	i = 0;
-	while (str[i] != set[0])
-		i++;
-	if (strchr(set, str[i]) != 0)
-		i++;
-	if (i == ft_strlen(str))
-		return (1);
-	else
-		return (0);
-}
-
-char				*ft_strtrim(char const *s1, char const *set)
-{
-	char			*s2;
-	unsigned int	len;
-	unsigned int	i;
-	unsigned int	j;
+	char				*s2;
+	unsigned long int	len;
+	unsigned int		i;
+	unsigned int		j;
 
 	if (!s1 || !set)
-		return(NULL);
+		return (NULL);
 	len = ft_strlen(set);
 	s2 = (char *)malloc(ft_strlen(s1) - len + 1);
+	if (!s2)
+		return (NULL);
 	i = 0;
-	while (strchr(set, s1[i], len) != 0 && s1[i] != '\0')
+	while (ft_strchr(set, s1[i]) != 0 && s1[i] != '\0')
 		i++;
-
-	while(s1[i] != '\0' && )
-	j = 0;
-	while (s1[i] != '\0' && strncmp(s1, set, len) != 0 && check_endset(s1, set))
-	{
-		s2[j] = s1[i];
-		i++;
+	j = i;
+	i--;
+	while (s1[j] != '\0')
 		j++;
-	}
+	j--;
+	while (ft_strchr(set, s1[j]) != 0 && j != i)
+		j--;
+	if (j != i)
+		s2 = ft_substr(s1, i + 1, j - i);
 	s2[j] = '\0';
 	return (s2);
 }
